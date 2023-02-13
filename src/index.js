b9b57ec168720cba602c632d82888caef5475250
Okellothomas <thomasolal33@gmail.com>
2023-02-13 21:55:43 +0300

    change more

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/**
+ * linking bootstrap
+ */
+
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import "../node_modules/react-bootstrap/dist/react-bootstrap"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
