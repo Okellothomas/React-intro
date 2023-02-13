8cc32e725e566d40eef10293e8fa3471864ad059
Okellothomas <thomasolal33@gmail.com>
2023-02-13 22:00:42 +0300

    add more components

---
+++ src/components/Carousel.js
@@ -0,0 +1,51 @@
+import React from "react";
+import skate from "../images/skate.jpg"
+import smile from "../images/smile.jpg"
+import song from "../images/song.jpg"
+import "../index.css"
+
+import Carousel from 'react-bootstrap/Carousel';
+
+export default function Carousel() {
+  return (
+    <Carousel fade>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={song}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={smile}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={skate}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
